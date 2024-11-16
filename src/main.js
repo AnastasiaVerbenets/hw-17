b1903a40af6532634c079c2fa35106b825a7ffb1
anastasia <anastasiaverbenets@gmail.com>
2024-11-16 17:07:11 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,42 +1,30 @@
-import NewsApiService from "./js/new-service";
+const API_KEY = '04f715244097a0c4a22752c3612f0538';
+const BASE_URL = `https://api.themoviedb.org/3`;
 
-const form = document.getElementById('form');
-const button = document.getElementById('load-more__btn');
-const list = document.getElementById('list');
+const list = document.querySelector('.list');
 
-const newsApiService = new NewsApiService();
+fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`).then(response => {
+    if (!response.ok) {
+        throw new Error('error');
+    }
+    return response.json();
+}).then(data => renderMovies(data.results.slice(0, 5)))
+    .catch(error => console.log(error));
 
-form.addEventListener('submit', onSearch);
-button.addEventListener('click', onLoadMore);
+function renderMovies(movies) {
+    const markUp = movies.map(movie => {
+        // const imgUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
 
-function onSearch(e) {
-    e.preventDefault();
-
-    newsApiService.query = e.currentTarget.elements.query.value;
-
-    newsApiService.resetPage();
-    newsApiService.fetchArticles().then(renderNews);
-}
-
-function onLoadMore() {
-    newsApiService.fetchArticles().then(renderNews);
-}
-
-function renderNews(articles) {
-    const markup = articles.map(article => {
         return `
-        <li>
-            <a href="${article.url}" target="_blank" rel="noopener noreferrer">
-            <article>
-                <img src="${article.urlToImage}" alt="" width="480">
-                <h2>${article.title}</h2>
-                <p>Posted by: ${article.author}</p>
-                <p></p>
-            </article>
-            </a>
-        </li>
-        `
+    <li class='thumb'>
+    <img src='https://image.tmdb.org/t/p/w500/${movie.poster_path}' alt='poster' width='200'>
+        <h2>${movie.original_title}</h2>
+        <p>Original language: ${movie.original_language}</p>
+        <p>Release date: ${movie.release_date}</p>
+        <p>Origin country: ${movie.origin_country}</p>
+    </li>
+    `;
     }).join('');
 
-    list.insertAdjacentHTML('beforeend', markup);
+    list.insertAdjacentHTML('beforeend', markUp);
 }
