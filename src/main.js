843ee27a3ea8ac15d78099663cfd6d7fd2826f54
anastasia <anastasiaverbenets@gmail.com>
2024-11-17 01:28:02 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
-const API_KEY = '04f715244097a0c4a22752c3612f0538';
-const BASE_URL = `https://api.themoviedb.org/3`;
+import MovieApiService from "./js/movies-service";
+
+const movieApiService = new MovieApiService();
 
 const list = document.querySelector('.list');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`).then(response => {
-    if (!response.ok) {
-        throw new Error('error');
-    }
-    return response.json();
-}).then(data => renderMovies(data.results.slice(0, 5)))
-    .catch(error => console.log(error));
+loadMovies();
+
+prevBtn.addEventListener('click', onPrevPage);
+nextBtn.addEventListener('click', onNextPage);
+
+function loadMovies() {
+    movieApiService
+        .fetchMovies()
+        .then(movies => renderMovies(movies.slice(0, 9)))
+        .catch(error => console.log(error));
+}
 
 function renderMovies(movies) {
     const markUp = movies.map(movie => {
-        // const imgUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-
         return `
     <li class='thumb'>
-    <img src='https://image.tmdb.org/t/p/w500/${movie.poster_path}' alt='poster' width='200'>
+    <img src='https://image.tmdb.org/t/p/w500/${movie.poster_path}' alt='${movie.title}' width='400'>
         <h2>${movie.original_title}</h2>
         <p>Original language: ${movie.original_language}</p>
         <p>Release date: ${movie.release_date}</p>
@@ -27,4 +32,26 @@ function renderMovies(movies) {
     }).join('');
 
     list.insertAdjacentHTML('beforeend', markUp);
+}
+
+function onPrevPage() {
+    movieApiService.decrementPage();
+    clear();
+    loadMovies();
+    toggleBtn();
+}
+
+function onNextPage() {
+    movieApiService.incrementPage();
+    clear();
+    loadMovies();
+    toggleBtn();
+}
+
+function clear() {
+    list.innerHTML = '';
+}
+
+function toggleBtn() {
+    prevBtn.disabled = movieApiService.page <= 1;
 }
