5909a5fc8f78309f6ff5a56fd0ebe651e71719a6
AnastasiaVerbenets <anastasiaverbenets@gmail.com>
2024-11-20 19:59:40 +0000

    Deploying to gh-pages from @ AnastasiaVerbenets/hw-17@2a77cd4bc8ac28cc30049b369a996b806146514e 🚀

---
+++ assets/index-6319a193.js
@@ -0,0 +1,8 @@
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const l="47021183-57cb9b4788280b138c9bad41f",a="https://pixabay.com/api/";class u{constructor(){this.page=1}fetchImages(){const s=`${a}key=${l}&editors_choice=true&page=${this.page}&per_page=3`;return fetch(s).then(t=>{if(!t.ok)throw new Error(error);t.json()}).then(({hits:t})=>(this.incrementPage(),t))}incrementPage(){this.page+=1}}const d=new u,f=document.querySelector(".list"),p=document.querySelector(".load-more__btn");i();p.addEventListener("click",g);function i(){d.fetchImages().then(o=>m(o)).catch(o=>console.log(o))}function m(o){const s=o.map(t=>`
+        <li class="item">
+            <img src="${t.largeImageURL}" alt="${t.type}" />
+            <p class="user">User: ${t.user}</p>
+            <p class="likes">Likes: ${t.likes}</p>
+            <p class="comments">Comments: ${t.comments}</p>
+        </li>
+        `).join("");f.insertAdjacentHTML("beforeend",s)}function g(){i()}
--- assets/index-f0c19e3e.js
+++
@@ -1,8 +0,0 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const a="47021183-57cb9b4788280b138c9bad41f",u="https://pixabay.com/api";class f{constructor(){this.page=1}fetchImages(){const s=`${u}?key=${a}`;return fetch(s).then(t=>{if(!t.ok)throw new Error(error);return t.json()}).then(({hits:t})=>(this.incrementPage(),t))}incrementPage(){this.page+=1}}const i=new f,d=document.querySelector(".list"),m=document.querySelector(".load-more__btn");p();m.addEventListener("click",h);function p(){i.fetchImages().then(o=>l(o.slice(0,3))).catch(o=>console.log(o))}function l(o){const s=o.map(t=>`
-        <li class="item">
-            <img src="${t.largeImageURL}" alt="${t.type}" />
-            <p class="user">User: ${t.user}</p>
-            <p class="likes">Likes: ${t.likes}</p>
-            <p class="comments">Comments: ${t.comments}</p>
-        </li>
-        `).join("");d.insertAdjacentHTML("beforeend",s)}function h(){i.fetchImages().then(l)}
